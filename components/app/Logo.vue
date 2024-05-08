80e7f36c97a93430da692200429376229c67bd8e
Rajeev R Sharma <i.rarsh@gmail.com>
2024-05-09 00:15:06 +0530

    feat: add appicon and logo

---
+++ components/app/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <ULink
+    :to="nonClickable ? undefined : '/'"
+    class="inline-flex items-center whitespace-nowrap gap-2"
+    :as="nonClickable ? 'div' : undefined"
+  >
+    <AppIcon :size="logoSize" />
+    <span :class="textClasses" class="leading-none font-bold">
+      StoryBlobs
+    </span>
+  </ULink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  textClasses: {
+    type: String,
+    default: 'text-2xl',
+  },
+  logoSize: {
+    type: Number,
+    default: 36,
+  },
+  nonClickable: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
