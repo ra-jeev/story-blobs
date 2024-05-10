0b5bf3e289f9f40757921d00b40b227c37c2854b
Rajeev R Sharma <i.rarsh@gmail.com>
2024-05-11 00:55:07 +0530

    feat: add utils functions for form validation

---
+++ utils/index.ts
@@ -0,0 +1,25 @@
+export const toTitleCase = (text: string) => {
+  return `${text[0].toUpperCase()}${text.slice(1)}`;
+};
+
+export const validateField = (
+  path: string,
+  value: string,
+  minLen: number,
+  maxLen: number
+) => {
+  const titledPath = toTitleCase(path);
+  if (!value) {
+    return { path, message: `${titledPath} is required.` };
+  } else if (value.length < minLen) {
+    return {
+      path,
+      message: `${titledPath} can not be less than ${minLen} characters.`,
+    };
+  } else if (value.length > maxLen) {
+    return {
+      path,
+      message: `${titledPath} can not be more than ${maxLen} characters.`,
+    };
+  }
+};
