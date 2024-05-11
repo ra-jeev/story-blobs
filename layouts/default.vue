f75c1f68ecf9126c2f7fe964e346a9dbf98029e4
Rajeev R Sharma <i.rarsh@gmail.com>
2024-05-11 22:37:02 +0530

    feat: update layouts and add new ones

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <AppBar />
-    <main>
-      <slot />
-    </main>
+    <UContainer class="flex gap-x-4 py-4">
+      <AppSideBar class="w-72" />
+      <main class="p-4 w-full">
+        <slot />
+      </main>
+    </UContainer>
   </div>
 </template>
---
+++ layouts/story.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <AppBar />
+    <UContainer class="flex justify-center py-4">
+      <main class="w-full max-w-lg">
+        <slot />
+      </main>
+    </UContainer>
+  </div>
+</template>
