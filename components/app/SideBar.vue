69b53936c8314ff642cece01eca4efae430bda82
Rajeev R Sharma <i.rarsh@gmail.com>
2024-05-11 22:32:33 +0530

    feat: add homepage sidebar

---
+++ components/app/SideBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <UCard>
+      <div class="text-lg md:text-xl font-semibold">Add your story</div>
+
+      <p class="mt-2 text-gray-500 dark:text-gray-400">
+        Let your creative juices flow...
+      </p>
+      <UButton class="mt-6" @click="navigateTo('/new')">Start a story</UButton>
+    </UCard>
+  </div>
+</template>
